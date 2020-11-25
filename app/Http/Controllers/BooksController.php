a25ec02b2f4cfd8ac483ff4c1b846e62ce504bc5
Luccas <luccas@comp.ufla.br>
2020-11-25 16:30:52 -0300

    add book resources

---
+++ app/Http/Controllers/BooksController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BooksController extends Controller
+{
+    public function store(){
+
+        // $data = $this->validateRequest();
+
+        Book::create($this->validateRequest());
+    }
+
+    public function update(Book $book){
+
+        // $data = $this->validateRequest();
+
+        $book->update($this->validateRequest());
+
+    }
+
+    protected function validateRequest(){
+         return request()->validate([
+             'title' => 'required',
+             'author' => 'required',
+         ]);
+    }
+}
